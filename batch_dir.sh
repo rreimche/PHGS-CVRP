f3e613e8ef8639ecde59d9c9ee25318d39e797dd
Roman Reimche <roman@romanreimche.com>
2023-06-26 18:56:39 +0200

    Add batch processing of batch processing

---
+++ batch_dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+num_subdirectories=$1
+
+# Create subdirectories if they don't exist
+for ((i = 1; i <= num_subdirectories; i++)); do
+  dir="$i"
+  if [ ! -d "$dir" ]; then
+    mkdir "$dir"
+  fi
+done
+
+# Execute the desired command in each subdirectory
+for dir in */; do
+  if [ -d "$dir" ]; then
+    cd "$dir" || exit
+    ../../batch.sh ../../build/hgs ../../Instances/CVRP
+    cd ..
+  fi
+done
